7f0ec5094f644b0046e5580af161e5c8beb986a6
Nicholas Hart <nickhart@gmail.com>
2025-09-02 21:20:16 -0700

    fixed lint and format issues

--- TimeTracker/TimeTrackerApp.swift
+++ TimeTracker/TimeTrackerApp.swift
@@ -1,20 +1,13 @@
-//
-//  TimeTrackerApp.swift
-//  TimeTracker
-//
-//  Created by Nick Hart on 9/2/25.
-//
-
 import SwiftUI
 
 @main
 struct TimeTrackerApp: App {
-    let persistenceController = PersistenceController.shared
+  let persistenceController = PersistenceController.shared
 
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+        .environment(\.managedObjectContext, self.persistenceController.container.viewContext)
     }
+  }
 }
--- TimeTrackerTests/TimeTrackerTests.swift
+++ TimeTrackerTests/TimeTrackerTests.swift
@@ -1,17 +1,9 @@
-//
-//  TimeTrackerTests.swift
-//  TimeTrackerTests
-//
-//  Created by Nick Hart on 9/2/25.
-//
-
 import Testing
 @testable import TimeTracker
 
 struct TimeTrackerTests {
-
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-    }
-
+  @Test
+  func example() async throws {
+    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+  }
 }
